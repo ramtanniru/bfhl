04c2f18bd3afe1e8b11ac9e3fd10eb92407b60d6
ram_tanniru <ramtanniru2@gmail.com>
2024-08-25 17:59:16 +0530

    removed CORS err

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://bfhl-frontend-psi-seven.vercel.app/"}})
+CORS(app)
 @app.route('/')
 def home():
     return "Hello world"
